ac7ed8f93ab7459c932534258cbbea15189cb740
liukefu2050 <liukefu2050@sina.com>
2020-01-01 09:43:24 +0800

    example rn 59

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,7 +3,8 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.mg365.call_record.CallRecordPackage;
+import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CallRecordPackage()
+            new WebViewBridgePackage(),
+            new RNCWebViewPackage()
       );
     }
 
