1423586ef848ebb5eccec6fa463a5ed762bb6e69
liukefu2050 <liukefu2050@sina.com>
2020-06-28 18:09:16 +0800

    v2.0.0

--- editorExample/App.js
+++ editorExample/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
-import {Appearance, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+    Appearance, StyleSheet, Text, TouchableOpacity, View, KeyboardAvoidingView,
+    SafeAreaView,
+    ScrollView,
+} from 'react-native';
 import {RichEditor, RichToolbar} from 'react-native-editor';
 
 const initHTML = `<br/>
@@ -12,154 +16,145 @@ const initHTML = `<br/>
 
 
 export default class RichTextExample extends Component {
-  richText = React.createRef();
-  constructor(props) {
-    super(props);
-    const that = this;
-    const theme = props.theme || Appearance.getColorScheme();
-    const contentStyle = that.createContentStyle(theme);
-    that.state = {theme: theme, contentStyle};
-    //that.onHome = ::that.onHome;
-    //that.save = ::that.save;
-    //that.onTheme = ::that.onTheme;
-    //that.onPressAddImage = ::that.onPressAddImage;
-    //that.themeChange = ::that.themeChange;
-  }
+    richText = React.createRef();
 
-  onPressInsert=()=>{
-    //this.richtext.setContentHTML("<p>insert by setContentHTML</p>");
-  }
-
-  onPressAppendInsert=()=>{
-    //this.richtext.appendContentHTML("<p>append by appendContentHTML</p>");
-  }
-
-  onPressGotFocus=()=>{
-      //this.richtext.focusContent();
-  }
-
-  onPressAddImage=()=> {
-    // insert URL
-    this.richText.current?.insertImage(
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png',
-    );
-    // insert base64
-    // this.richText.current?.insertImage(`data:${image.mime};base64,${image.data}`);
-    this.richText.current?.blurContentEditor();
-  }
-
-  createContentStyle=(theme)=>  {
-    const contentStyle = {backgroundColor: '#000033', color: '#fff', placeholderColor: 'gray'};
-    if (theme === 'light') {
-      contentStyle.backgroundColor = '#fff';
-      contentStyle.color = '#000033';
-      contentStyle.placeholderColor = '#a9a9a9';
+    constructor(props) {
+        super(props);
+        const that = this;
+        const theme = props.theme || Appearance.getColorScheme();
+        const contentStyle = that.createContentStyle(theme);
+        that.state = {theme: theme, contentStyle};
+        //that.onHome = ::that.onHome;
+        //that.onPressGotValue = ::that.onPressGotValue;
+        //that.onTheme = ::that.onTheme;
+        //that.onPressAddImage = ::that.onPressAddImage;
+        //that.themeChange = ::that.themeChange;
     }
-    return contentStyle;
-  }
 
-  async save() {
-    // Get the data here and call the interface to save the data
-    let html = await this.richText.current?.getContentHtml();
-    // console.log(html);
-    alert(html);
-  }
+    onPressInsert = () => {
+        this.richText.setContentHTML("<p>insert by setContentHTML</p>");
+    }
 
-  render() {
-    let that = this;
-    const {contentStyle, theme} = that.state;
-    const {backgroundColor, color, placeholderColor} = contentStyle;
-    const themeBg = {backgroundColor};
-    return (
-        <View style={styles.container}>
-          <View style={{flexDirection: 'row',height:50,        alignItems: 'center',
-            justifyContent: 'center',backgroundColor:'#847b66'}}>
-            <TouchableOpacity style={{marginLeft:20,padding:5}} onPress={this.onPressInsert}>
-              <Text>insert HTML</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{marginLeft:20,padding:5}} onPress={this.onPressAppendInsert}>
-              <Text>append HTML</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={{marginLeft:20,padding:5}} onPress={this.onPressGotFocus}>
-              <Text>got Focus</Text>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <RichEditor
-                editorStyle={contentStyle}
-                containerStyle={themeBg}
-                ref={that.richText}
-                style={[styles.rich, themeBg]}
-                placeholder={'please input content'}
-                initialContentHTML={initHTML}
-            />
-            <RichToolbar
-                style={[styles.richBar, themeBg]}
-                editor={that.richText}
-                iconTint={color}
-                selectedIconTint={'#2095F2'}
-                selectedButtonStyle={{backgroundColor: 'transparent'}}
-                onPressAddImage={that.onPressAddImage}
-            />
-          </View>
+    onPressAppendInsert = () => {
+        //this.richText.appendContentHTML("<p>append by appendContentHTML</p>");
+    }
 
-        </View>
-    );
-  }
+    onPressGotFocus() {
+        this.richText.focusContentEditor();
+    }
 
-  onEditorInitialized() {
-    this.setFocusHandlers();
-    this.getHTML();
-  }
+    onPressAddImage() {
+        // insert URL
+        this.richText.current?.insertImage(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png',
+        );
+        // insert base64
+        // this.richText.current?.insertImage(`data:${image.mime};base64,${image.data}`);
+        this.richText.current?.blurContentEditor();
+    }
 
-  async getHTML() {
-    const titleHtml = await this.richtext.getTitleHtml();
-    const contentHtml = await this.richtext.getContentHtml();
-    //alert(titleHtml + ' ' + contentHtml)
-  }
+    createContentStyle (theme){
+        const contentStyle = {backgroundColor: '#000033', color: '#fff', placeholderColor: 'gray'};
+        if (theme === 'light') {
+            contentStyle.backgroundColor = '#fff';
+            contentStyle.color = '#000033';
+            contentStyle.placeholderColor = '#a9a9a9';
+        }
+        return contentStyle;
+    }
 
-  setFocusHandlers() {
-    this.richtext.setTitleFocusHandler(() => {
-      //alert('title focus');
-    });
-    this.richtext.setContentFocusHandler(() => {
-      //alert('content focus');
-    });
-  }
+    async onPressGotValue() {
+        // Get the data here and call the interface to save the data
+        let html = await this.richText.current?.getContentHtml();
+        // console.log(html);
+        alert(html);
+    }
+
+    render() {
+        let that = this;
+        const {contentStyle, theme} = that.state;
+        const {backgroundColor, color, placeholderColor} = contentStyle;
+        const themeBg = {backgroundColor};
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={{flexDirection: 'row', height: 50, alignItems: 'center',justifyContent: 'center', backgroundColor: '#847b66'}}>
+                    <TouchableOpacity style={{marginLeft: 10, padding: 5}} onPress={this.onPressInsert}>
+                        <Text>insertHTML</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: 10, padding: 5}} onPress={this.onPressAppendInsert}>
+                        <Text>appendHTML</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{marginLeft: 10, padding: 5}} onPress={this.onPressGotFocus}>
+                        <Text>gotFocus</Text>
+                    </TouchableOpacity>
+                  <TouchableOpacity style={{marginLeft: 10, padding: 5}} onPress={this.onPressGotValue}>
+                    <Text>gotValue</Text>
+                  </TouchableOpacity>
+                </View>
+                <ScrollView style={[styles.scroll, themeBg]} keyboardDismissMode={'none'}>
+                    <RichEditor
+                        editorStyle={contentStyle}
+                        containerStyle={themeBg}
+                        ref={that.richText}
+                        style={[styles.rich, themeBg]}
+                        placeholder={'please input content'}
+                        initialContentHTML={initHTML}
+                    />
+                </ScrollView>
+                <RichToolbar
+                    style={[styles.richBar, themeBg]}
+                    editor={that.richText}
+                    iconTint={color}
+                    selectedIconTint={'#2095F2'}
+                    selectedButtonStyle={{backgroundColor: 'transparent'}}
+                    onPressAddImage={that.onPressAddImage}
+                />
+
+            </SafeAreaView>
+        );
+    }
+
+    onEditorInitialized() {
+        this.setFocusHandlers();
+        this.getHTML();
+    }
+
+    async getHTML() {
+        const titleHtml = await this.richText.getTitleHtml();
+        const contentHtml = await this.richText.getContentHtml();
+        //alert(titleHtml + ' ' + contentHtml)
+    }
+
+    setFocusHandlers() {
+        this.richText.setTitleFocusHandler(() => {
+            //alert('title focus');
+        });
+        this.richText.setContentFocusHandler(() => {
+            //alert('content focus');
+        });
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-  nav: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 5,
-  },
-  rich: {
-    minHeight: 300,
-    flex: 1,
-  },
-  richBar: {
-    height: 50,
-    backgroundColor: '#F5FCFF',
-  },
-  scroll: {
-    backgroundColor: '#ffffff',
-  },
-  item: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: '#eee',
-    flexDirection: 'row',
-    height: 40,
-    alignItems: 'center',
-    paddingHorizontal: 15,
-  },
-
-  input: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+    },
+    rich: {
+        minHeight: 300,
+        flex: 1,
+    },
+    richBar: {
+        height: 50,
+        bottom:0,
+        backgroundColor: '#F5FCFF',
+    },
+    scroll: {
+      flex: 1,
+        backgroundColor: '#ffffff',
+    },
+    input: {
+        flex: 1,
+    },
 });
 
